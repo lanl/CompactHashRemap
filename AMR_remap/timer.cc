a425baabe012579117b8010ae24f47e496f057e6
Bob Robey <brobey@lanl.gov>
2016-01-24 20:51:00 -0700

    Adding licensing info

--- AMR_remap/timer.cc
+++ AMR_remap/timer.cc
@@ -1,9 +1,31 @@
-/* LANL Copyright 2015
+/* Copyright 2015-16.  Los Alamos National Security, LLC. This material was produced
+ * under U.S. Government contract DE-AC52-06NA25396 for Los Alamos National 
+ * Laboratory (LANL), which is operated by Los Alamos National Security, LLC
+ * for the U.S. Department of Energy. The U.S. Government has rights to use,
+ * reproduce, and distribute this software.  NEITHER THE GOVERNMENT NOR LOS
+ * ALAMOS NATIONAL SECURITY, LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
+ * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  If software is modified
+ * to produce derivative works, such modified software should be clearly marked,
+ * so as not to confuse it with the version available from LANL.   
  *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at 
  *
- *  Authors: 
- *        Gerald Collom        gcollom@lanl.gov
- *        Colin Redman        credman@lanl.gov
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Under this license, it is required to include a reference to this work.
+ *
+ * This is LANL Copyright Disclosure C16017/LA-CC-15-102
+ *
+ * Authors: Bob Robey         XCP-2   brobey@lanl.gov
+ *          Gerald Collom     XCP-2   gcollom@lanl.gov
+ *          Colin Redman      XCP-2   credman@lanl.gov 
  */
 
 // Timer routines to simplify main loop
